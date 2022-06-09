610d7449515f6c25eaf6f2800c4a1bad4d6252fb
Abrprime <abr123ayalew@gmail.com>
2022-06-09 09:58:21 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - print number
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		if (c == 50 || c == 52)
+			continue;
+		_putchar(c);
+	}
+	_putchar(10);
+}
