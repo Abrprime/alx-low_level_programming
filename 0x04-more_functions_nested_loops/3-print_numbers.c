95156ebd36edad555eaca6f8786039274484f832
Abrprime <abr123ayalew@gmail.com>
2022-06-09 09:24:32 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ *
+ * Return: rterun value
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar('\n');
+	return ();
+}
