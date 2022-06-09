b22cc4453cf21659c323e209effad05f597f2b62
Abrprime <abr123ayalew@gmail.com>
2022-06-09 03:12:56 -0700

    task0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+gcc -E $CFILE -o c
+gcc -c $CFILE
+gcc -S $CFILE
+gcc $CFILE -o cisfun
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: 0 number
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
