929dcd445b7e71eb173a4c35cbfe09a933f6c269
Abrprime <abr123ayalew@gmail.com>
2022-06-12 04:44:39 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - print putchart
+ *
+ * Reurn: return 0
+ */
+int main(void)
+{
+	char a[8] = '_putchar';
+	int i;
+
+	for (i = 0; i <= 0; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+	return (0);
+}
