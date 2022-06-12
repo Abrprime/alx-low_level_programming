6fe73fcb795f98ad6ad01b43874b75c576270efb
Abrprime <abr123ayalew@gmail.com>
2022-06-12 07:08:05 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - main function
+ *
+ * @int: parameter
+ * Return: value
+ *
+ */
+int _abs(int)
+{
+	unsigned int n;
+
+	if (n < 0)
+	{
+		n = n * -;
+		_putchar(n);
+	}
+	else
+		_putchar(n);
+}
