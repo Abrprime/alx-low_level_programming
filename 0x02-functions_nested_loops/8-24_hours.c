f05f217d5b9f5d05349f5f35a632f3ab948d9825
Abrprime <abr123ayalew@gmail.com>
2022-06-12 08:39:53 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,18 +12,39 @@ void jack_bauer(void)
 
 	for (a = 48; a < 51; a++)
 	{
-		for (b = 48;  b < 52; b++)
+		if (a < 49)
 		{
-			for (c = 48; c < 54; c++)
+			for (b = 48;  b < 58; b++)
 			{
-				for (d = 48; d < 58; d++)
+				for (c = 48; c < 54; c++)
 				{
-					_putchar(a);
-					_putchar(b);
-					_putchar(':');
-					_putchar(c);
-					_putchar(d);
-					_putchar(10);
+					for (d = 48; d < 58; d++)
+					{
+						_putchar(a);
+						_putchar(b);
+						_putchar(':');
+						_putchar(c);
+						_putchar(d);
+						_putchar(10);
+					}
+				}
+			}
+		}
+		else
+		{
+			for (b = 48;  b < 52; b++)
+			{
+				for (c = 48; c < 54; c++)
+				{
+					for (d = 48; d < 58; d++)
+					{
+						_putchar(a);
+						_putchar(b);
+						_putchar(':');
+						_putchar(c);
+						_putchar(d);
+						_putchar(10);
+					}
 				}
 			}
 		}
