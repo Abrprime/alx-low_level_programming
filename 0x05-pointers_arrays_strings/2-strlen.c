5a305eff75770e536dcce3b0a06bbf0f6233b676
Abrprime <abr123ayalew@gmail.com>
2022-06-13 08:51:09 -0700

    task 3

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,8 +6,11 @@
  */
 int _strlen(char *s)
 {
-	int n;
+	int n = 0;
 
-	n = sizeof(s);
-	return (n / 4);
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	return (n);
 }
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts - main function
+ *
+ * @str: parameter
+ *
+ * Return: string
+ */
+void _puts(char *str)
+{
+	return (str[], 10);
+}
