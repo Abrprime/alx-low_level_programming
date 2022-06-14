703af934f42cade73c0e718cf85cda805d687665
Abrprime <abr123ayalew@gmail.com>
2022-06-13 21:21:57 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,8 +11,15 @@ void puts_half(char *str)
 	for (n = 0; str[n] != '\0'; n++)
 
 	if (n % 2 == 0)
-		_putchar(str[(n / 2)++]);
+	{
+		n = (n / 2);
+		while (n > 0)
+			_putchar(str[n++]);
+	}
 	else
-		_putchar(str[((n - 1) / 2)++]);
+	{
+		n = (n - 1) / 2;
+		while (n > 0)
+			_putchar(str[n++]);
 	_putchar(10);
 }
