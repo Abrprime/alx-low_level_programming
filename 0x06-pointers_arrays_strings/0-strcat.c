5721671563ac8589a144d13107af8b43b24311c4
Abrprime <abr123ayalew@gmail.com>
2022-06-15 07:04:23 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *_strcat - main function
+ *
+ * @dest: parameter
+ * @src: parameter
+ *
+ * Return: dest
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = j = 0;
+
+	while (dest[i])
+	{
+		i++;
+	}
+	while (src[j])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
