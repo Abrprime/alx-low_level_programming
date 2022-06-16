61538bc2535f0c2df1500cc88664a5c2a31115de
Abrprime <abr123ayalew@gmail.com>
2022-06-16 02:32:13 -0700

    task3

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - main function
+ *
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && *(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
+	return (dest);
+}
