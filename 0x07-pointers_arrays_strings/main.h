5d84906a90e477fa7b6bc41a29862a5a336469b3
Abrprime <abr123ayalew@gmail.com>
2022-06-20 18:23:39 -0700

    task 8

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,3 +3,8 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 nsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
+#define NULL 0
+void print_diagsums(int *a, int size);
