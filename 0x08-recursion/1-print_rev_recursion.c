19e07f3833a9908bed62b793780cf4cdffe744f4
Abrprime <abr123ayalew@gmail.com>
2022-06-22 16:39:20 -0700

    task1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,14 +6,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		return;
-	}
-	else
+	if (*s != '\0')
 	{
 		_print_rev_recursion(++s);
-        	_putchar(*s);
+                _putchar(*s);
 	}
-	_putchar(10);
+	return;
 }
