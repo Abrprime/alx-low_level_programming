c563c8f50f66d704ce45a042f89f3a7bee2c8c1f
Abrprime <abr123ayalew@gmail.com>
2022-06-22 07:24:35 -0700

    task 1 fix

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _print_rev_recursion - main fuction
- * @s: parameter 
+ * @s: parameter
  * Retrun: void
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s = '/0')
+	if (*s != '\0')
 	{
 		_putchar(s + 1);
 		_print_rev_recursion(*s)
